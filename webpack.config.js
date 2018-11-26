16d3987c95daeccdae996b1f29a52b8835e66234
Chem-e <nooru612@gmail.com>
2018-11-26 11:26:40 +0000

    header updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,51 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
 module.exports = {
     entry: './js/script.js',
     output: {
-        filename: './js/bundle.js'
+        filename: './dist/bundle.js'
     },
     watch: true,
     module: {
         rules: [{
-            test: /\.css$/,
-            use: [
-                { loader: "style-loader" },
-                { loader: "css-loader" }
-            ]
-        }]
-    }
-};
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }, {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }, {
+                "test": /\.html$/,
+                use: {
+                    "loader": "raw-loader"
+                }
+            },
+            {
+                "test": /\.(eot|svg)$/,
+                "loader": "file-loader?name=[name].[hash:20].[ext]"
+            },
+            {
+                "test": /\.(jpg|png|gif|otf|ttf|woff|woff2|cur|ani)$/,
+                "loader": "url-loader?name=[name].[hash:20].[ext]&limit=10000"
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("./css/styles.css"),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+}
