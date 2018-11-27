0d2b6f39ba694266a95f573a909538d6a5229da9
Chem-e <nooru612@gmail.com>
2018-11-27 15:22:04 +0000

    our services

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
             },
             {
                 "test": /\.(jpg|png|gif|otf|ttf|woff|woff2|cur|ani)$/,
-                "loader": "url-loader?name=[name].[hash:20].[ext]&limit=10000"
+                "loader": "url-loader"
             },
         ]
     },
